69c4df099b0b0e53d3b85e72d06e0178a6ec9414
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-10-20 18:26:54 +0200

    Switch to OpenSSL

--- src/enc.cpp
+++ src/enc.cpp
@@ -4,7 +4,11 @@
 #include <cstdint>
 #include <cstring>
 #include <memory>
+#include <array>
 #include <filesystem>
+
+#include <openssl/evp.h>
+#include <openssl/err.h>
 
 namespace fs = std::filesystem;
 
@@ -16,226 +20,53 @@ enum class encrypt_type {
 
 encrypt_type type = encrypt_type::plain;
 
-struct mask {
-    unsigned char bytes[16];
-};
+using key_type = std::array<unsigned char, 16>;
+key_type counter_from_file_index(std::size_t file_index);
 
-mask incr_mask(mask m)
+auto const key = counter_from_file_index(0);
+
+void new_decrypt(key_type t, char* data, std::size_t size)
 {
-    for(int index = 15; index >= 0; --index) {
-        if(m.bytes[index] != 0xff) {
-            ++m.bytes[index];
-            break;
-        }
-
-        m.bytes[index] = 0;
+    auto cipher = EVP_aes_128_ctr();
+    auto context = EVP_CIPHER_CTX_new();
+    if (!context) {
+        ERR_print_errors_fp(stderr);
+        throw std::runtime_error{"Couldn't create cipher context."};
     }
 
-    return m;
-}
-
-struct encryption_state {
-    mask counter{};
-};
-
-mask const prepared_masks[11]{
-    {0xc3,0xd2,0xba,0xe7,0xc3,0xf3,0x62,0x9a,0x17,0x53,0x71,0xd6,0xb1,0xf5,0x05,0xaa},
-    {0x24,0xb9,0x16,0x2f,0xe7,0x4a,0x74,0xb5,0xf0,0x19,0x05,0x63,0x41,0xec,0x00,0xc9},
-    {0xe8,0xda,0xcb,0xac,0x0f,0x90,0xbf,0x19,0xff,0x89,0xba,0x7a,0xbe,0x65,0xba,0xb3},
-    {0xa1,0x2e,0xa6,0x02,0xae,0xbe,0x19,0x1b,0x51,0x37,0xa3,0x61,0xef,0x52,0x19,0xd2},
-    {0xa9,0xfa,0x13,0xdd,0x07,0x44,0x0a,0xc6,0x56,0x73,0xa9,0xa7,0xb9,0x21,0xb0,0x75},
-    {0x44,0x1d,0x8e,0x8b,0x43,0x59,0x84,0x4d,0x15,0x2a,0x2d,0xea,0xac,0x0b,0x9d,0x9f},
-    {0x4f,0x43,0x55,0x1a,0x0c,0x1a,0xd1,0x57,0x19,0x30,0xfc,0xbd,0xb5,0x3b,0x61,0x22},
-    {0xed,0xac,0xc6,0xcf,0xe1,0xb6,0x17,0x98,0xf8,0x86,0xeb,0x25,0x4d,0xbd,0x8a,0x07},
-    {0x17,0xd2,0x03,0x2c,0xf6,0x64,0x14,0xb4,0x0e,0xe2,0xff,0x91,0x43,0x5f,0x75,0x96},
-    {0xc3,0x4f,0x93,0x36,0x35,0x2b,0x87,0x82,0x3b,0xc9,0x78,0x13,0x78,0x96,0x0d,0x85},
-    {0x65,0x98,0x04,0x8a,0x50,0xb3,0x83,0x08,0x6b,0x7a,0xfb,0x1b,0x13,0xec,0xf6,0x9e},
-};
-
-unsigned char translation_table[256] {
-    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5,
-    0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
-    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0,
-    0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
-    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC,
-    0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
-    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A,
-    0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
-    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0,
-    0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
-    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B,
-    0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
-    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85,
-    0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
-    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5,
-    0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
-    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17,
-    0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
-    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88,
-    0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
-    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C,
-    0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
-    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9,
-    0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
-    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6,
-    0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
-    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E,
-    0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
-    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94,
-    0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68,
-    0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
-};
-
-mask arbitrary_translate(mask m)
-{
-    for(auto& b : m.bytes) {
-        b = translation_table[b];
+    if (EVP_DecryptInit_ex(context, cipher, NULL, &key[0], &t[0]) != 1) {
+        ERR_print_errors_fp(stderr);
+        throw std::runtime_error{"Couldn't initialise context."};
     }
 
-    return m;
-}
+    std::unique_ptr<char[]> encrypted{new char[size]};
+    std::copy(data, data + size, encrypted.get());
 
-mask byte_shuffle(mask original)
-{
-    mask shuffled{original};
-
-    shuffled.bytes[0x1] = original.bytes[0x5];
-    shuffled.bytes[0x5] = original.bytes[0x9];
-    shuffled.bytes[0x9] = original.bytes[0xd];
-    shuffled.bytes[0xd] = original.bytes[0x1];
-    shuffled.bytes[0x2] = original.bytes[0xa];
-    shuffled.bytes[0xa] = original.bytes[0x2];
-    shuffled.bytes[0x6] = original.bytes[0xe];
-    shuffled.bytes[0xe] = original.bytes[0x6];
-    shuffled.bytes[0x3] = original.bytes[0xf];
-    shuffled.bytes[0xf] = original.bytes[0xb];
-    shuffled.bytes[0xb] = original.bytes[0x7];
-    shuffled.bytes[0x7] = original.bytes[0x3];
-
-    return shuffled;
-}
-
-std::uint32_t xorshift(std::uint32_t in)
-{
-    return ((in & 0xff) >> 7) * 0x1b ^ (in & 0xff) * 2;
-}
-
-// I HAVE ANGERED THE GODS
-mask xorify(mask original)
-{
-    auto ptr = original.bytes + 2;
-    unsigned char local7;
-    unsigned char local6;
-    unsigned char local5;
-    std::uint32_t localc;
-    for(int count = 0; count != 4; ++count) {
-        auto al = ptr[0x1];
-        auto cl = ptr[-0x2];
-        auto bh = ptr[0x0];
-        auto bl = ptr[-0x1];
-
-        local7 = al;
-
-        al ^= bh;
-
-        localc = al;
-
-        al ^= cl;
-        al ^= bl;
-
-        local6 = cl;
-        local5 = al;
-
-        al = cl;
-        al ^= bl;
-
-        al = xorshift(al);
-        al ^= local6;
-        al ^= local5;
-        ptr[-0x2] = al;
-
-        al = bl;
-        al ^= bh;
-
-        al = xorshift(al);
-
-        al ^= bl;
-        bl = local5;
-        al ^= bl;
-
-        ptr[-0x1] = al;
-
-        al = xorshift(localc);
-        al ^= bh;
-        al ^= bl;
-
-        ptr[0] = al;
-
-        al = local7;
-        al ^= local6;
-        al = xorshift(al);
-        al ^= local7;
-
-        ptr += 4;
-
-        al ^= bl;
-        ptr[-0x3] = al;
+    int len;
+    if (EVP_DecryptUpdate(
+            context,
+            reinterpret_cast<unsigned char*>(data), &len,
+            reinterpret_cast<unsigned char*>(encrypted.get()), size
+        ) != 1
+    ) {
+        ERR_print_errors_fp(stderr);
+        throw std::runtime_error{"Couldn't decrypt."};
     }
 
-    return original;
-}
-
-mask apply_prepared_mask(mask m, unsigned mask_select, encryption_state* t)
-{
-    auto mask_ptr = prepared_masks[mask_select & 0xff].bytes;
-    for(auto& byte : m.bytes) {
-        byte ^= *(mask_ptr++);
+    if(EVP_DecryptFinal_ex(context, reinterpret_cast<unsigned char*>(data + len), &len) != 1) {
+        ERR_print_errors_fp(stderr);
+        throw std::runtime_error{"Couldn't finalise decrypt."};
     }
 
-    return m;
+    EVP_CIPHER_CTX_free(context);
 }
 
-mask make_mask(mask counter, encryption_state* t)
-{
-    mask ret{counter};
-
-    ret = apply_prepared_mask(ret, 0, t);
-
-    // do this nine times for good measure..?
-    for(int count = 1; count <= 9; ++count) {
-        ret = arbitrary_translate(ret);
-        ret = byte_shuffle(ret);
-        ret = xorify(ret);
-        ret = apply_prepared_mask(ret, count, t);
-    }
-
-    ret = arbitrary_translate(ret);
-    ret = byte_shuffle(ret);
-    ret = apply_prepared_mask(ret, 10, t);
-
-    return ret;
-}
-
-void decrypt(encryption_state* t, char* data, std::size_t size)
+void decrypt(key_type t, char* data, std::size_t size)
 {
     if (type == encrypt_type::plain)
         return;
 
-    mask current_mask{};
-    std::size_t mask_index{16};
-
-    for(std::size_t index{0}; index < size; ++index) {
-        if(mask_index == 16) {
-            current_mask = make_mask(t->counter, t);
-            t->counter = incr_mask(t->counter);
-
-            mask_index = 0;
-        }
-
-        auto mask = current_mask.bytes[mask_index++];
-        data[index] ^= mask;
-    }
+    new_decrypt(t, data, size);
 }
 
 std::uint32_t load_le_uint32(const char* const ptr)
@@ -270,7 +101,7 @@ struct rng {
     }
 };
 
-mask counter_from_file_index(std::size_t file_index)
+key_type counter_from_file_index(std::size_t file_index)
 {
     auto seed = static_cast<double>(23456911 + file_index);
 
@@ -287,11 +118,10 @@ mask counter_from_file_index(std::size_t file_index)
         n = -r.next();
     }
 
-
-    mask counter{};
+    key_type counter{};
 
     static_assert(sizeof(numbers) == sizeof(counter));
-    std::memcpy(&counter, numbers, sizeof(counter));
+    std::memcpy(&counter[0], numbers, sizeof(counter));
 
     return counter;
 }
@@ -322,14 +152,13 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    const encryption_state initial_header{
+    const key_type initial_header{
         counter_from_file_index(1)
     };
 
-    const encryption_state initial_file_table{
+    const key_type initial_file_table{
         counter_from_file_index(0x7ffffffe)
     };
-
 
     const auto output_path = fs::path{argv[3]};
     if(!fs::is_directory(output_path)) {
@@ -354,7 +183,7 @@ int main(int argc, char** argv)
     const auto header = memory.get();
     const auto header_size = 0x10;
     auto header_state = initial_header;
-    decrypt(&header_state, header, header_size);
+    decrypt(header_state, header, header_size);
 
     std::cout << "Header: ";
     std::cout
@@ -369,7 +198,7 @@ int main(int argc, char** argv)
     const auto file_table_size = load_le_uint32(header + 8) - header_size;
 
     auto file_table_state = initial_file_table;
-    decrypt(&file_table_state, file_table, file_table_size);
+    decrypt(file_table_state, file_table, file_table_size);
 
     const auto* file_table_entry = file_table;
 
@@ -400,11 +229,11 @@ int main(int argc, char** argv)
         const auto counter = counter_from_file_index(file_index);
         const auto file_contents = header + file_offset;
 
-        encryption_state file_encryption_state{
+        key_type file_encryption_state{
             counter
         };
 
-        decrypt(&file_encryption_state, file_contents, file_size);
+        decrypt(file_encryption_state, file_contents, file_size);
 
         const auto file_path = output_path / file_name;
         fs::create_directories(file_path.parent_path());
